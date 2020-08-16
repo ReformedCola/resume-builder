68810e9e7eedde84454f829a96c4f3f56171d029
ReformedCola <reformedcola@gmail.com>
2020-08-16 15:12:05 -0700

    deploy

---
+++ precache-manifest.fca62d1cb0b25ef3e06ac683bf19cc41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f9c4e998c0e654e4373",
+    "url": "/css/app.40701c3d.css"
+  },
+  {
+    "revision": "e7392c2b27010b22de4ac36b5393c548",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f9c4e998c0e654e4373",
+    "url": "/js/app.9948777f.js"
+  },
+  {
+    "revision": "64a47bfd7531f1282d5d",
+    "url": "/js/chunk-vendors.9df43ae7.js"
+  },
+  {
+    "revision": "205214b630931258b409679d5e239827",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
